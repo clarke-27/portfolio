c43eccbd9b2aa2b73a49211540475b0751a761c9
clarke-27 <clarkeoscar27@protonmail.com>
2018-09-12 21:45:23 +1000

    Portfolio for CDM SAT #3
    A portfolio by Oscar Clarke

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('#overlay').hide();
+  $('#openNav').click(function () {
+    $('#overlay').slideDown(600, 'swing');
+  });
+
+  $('#exit').click(function () {
+    $('#overlay').slideUp(600, 'swing');
+  });
+});
